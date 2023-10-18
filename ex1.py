f53c40375fb973867089901c543f7b7e1439b1fb
mthehang <matheus@vts.com.br>
2023-10-18 09:27:34 -0300

    teste

---
+++ ex1.py
@@ -0,0 +1,4 @@
+
+valor = float(input('Digite o valor do produto R$'))
+parcelas = int(input(f'Em quantas vezes vai parcelar o valor de R${valor}? ')
+print(f'O valor de cada parcela sera R${valor/parcelas}')
