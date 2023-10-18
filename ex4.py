bd1887854790950824e5120228577994eae4f3f0
mthehang <matheus@vts.com.br>
2023-10-18 10:56:55 -0300

    ex4

---
+++ ex4.py
@@ -0,0 +1,5 @@
+
+nome = input('Digite o nome do aluno: ')
+nota1 = float(input('Digite a nota da primeira prova: '))
+nota2 = float(input('Digite a nota da segunda prova: '))
+print(f'A media do(a) {nome} e: {(nota1 + nota2)/2}')
