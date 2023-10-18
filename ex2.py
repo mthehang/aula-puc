3976378b9d3736c4935b16bfbf961e002f218dd6
mthehang <matheus@vts.com.br>
2023-10-18 10:18:42 -0300

    exs sala

---
+++ ex2.py
@@ -0,0 +1,9 @@
+A = int(input("digite um numero: "))
+B = int(input("digite outro numero: "))
+
+if A > B:
+    print(f"{A} e maior que {B}")
+elif A < B:
+    print(f"{A} e menor que {B}")
+else:
+    print(f"{A} e {A} sao iguais")
