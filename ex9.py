f74f7504e660a769a94a277af2ccc6e4bc8b1317
mthehang <matheus@vts.com.br>
2023-10-19 10:35:24 -0300

    operador not

--- ex9.py
+++ ex9.py
@@ -1,7 +1,7 @@
 
 numero = int(input('Digite um numero: '))
 
-if numero >= 8 and numero <=256 or numero >= 300 and numero <= 512:
+if not numero < 8 and not numero > 256 or not numero < 300 and not numero > 512:
     print('O numero esta entre os intervalos definidos.')
 else:
     print('O numero nao esta nos intervalos')
