1552391205b4dd0adc1171595dcbe261abc897d8
mthehang <matheus@vts.com.br>
2023-10-19 10:43:49 -0300

    Acentuação

--- ex9.py
+++ ex9.py
@@ -1,7 +1,7 @@
 
-numero = int(input('Digite um numero: '))
+numero = int(input('Digite um número: '))
 
 if not numero < 8 and not numero > 256 or not numero < 300 and not numero > 512:
-    print('O numero esta entre os intervalos definidos.')
+    print('O número está entre os intervalos definidos.')
 else:
-    print('O numero nao esta nos intervalos')
+    print('O número não está nos intervalos')
