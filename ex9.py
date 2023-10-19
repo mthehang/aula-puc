52bd3356bc60e50f31f2c34ed50cc953b6fcb599
mthehang <matheus@vts.com.br>
2023-10-19 09:39:42 -0300

    ex9

---
+++ ex9.py
@@ -0,0 +1,7 @@
+
+numero = int(input('Digite um numero: '))
+
+if numero >= 8 and numero <=256 or numero >= 300 and numero <= 512:
+    print('O numero esta entre os intervalos definidos.')
+else:
+    print('O numero nao esta nos intervalos')
