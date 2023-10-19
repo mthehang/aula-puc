34882a031a5b3217e50d1e95960f24cead46bed5
mthehang <matheus@vts.com.br>
2023-10-19 10:52:00 -0300

    ex10

---
+++ ex10.py
@@ -0,0 +1,8 @@
+
+lista = []
+
+while len(lista) < 10:
+    numero = int(input('Digite um numero: '))
+    lista.append(numero)
+
+print(lista)
--- ex9.py
+++ ex9.py
@@ -4,4 +4,4 @@ numero = int(input('Digite um número: '))
 if not numero < 8 and not numero > 256 or not numero < 300 and not numero > 512:
     print('O número está entre os intervalos definidos.')
 else:
-    print('O número não está nos intervalo')
+    print('O número não está nos intervalos')
