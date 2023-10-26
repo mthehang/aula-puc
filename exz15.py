6919f9bb03ca5e8180f239c081712654518a78bb
mthehang <matheus@vts.com.br>
2023-10-26 08:47:05 -0300

    ex empty

--- exz15.py
+++ exz15.py
@@ -1,1 +1,18 @@
+import random
 
+lista = []
+
+while True:
+    try:
+        quantidade = int(input('Digite a quantidade de números aleatórios que deseja gerar: '))
+        if quantidade == 0:
+            print('Programa finalizado.')
+        break
+    except ValueError:
+        print('Quantidade inválida.')
+
+while len(lista) < quantidade:
+    lista.append(random.randrange(1, 100))
+
+for items in lista:
+    print(items)
