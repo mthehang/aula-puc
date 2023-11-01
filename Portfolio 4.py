ac6645e401c80d578731f1347cc7f82514c74af5
mthehang <matheus@vts.com.br>
2023-11-01 13:59:49 -0300

    portfolio 4

---
+++ Portfolio 4.py
@@ -0,0 +1,13 @@
+
+lista_arquivos1 = ["IM0001.PDF", "IM0002.DCM", "IM0003.DCM", "IM0004.PDF", "IM0005.PDF"]
+lista_arquivos2 = ["IM0001.PDF", "IM0002.DCM", "IM0004.PDF"]
+
+arquivos_presentes = [arquivo for arquivo in lista_arquivos1 if arquivo in lista_arquivos2]
+arquivos_faltando = [arquivo for arquivo in lista_arquivos1 if arquivo not in lista_arquivos2]
+
+# Imprimir os resultados
+print(f"Arquivos presentes em ambos (sistema e backup): {', '.join(arquivos_presentes)}")
+print(f"Total: {len(arquivos_presentes)}\n")
+
+print(f"Arquivos faltando no sistema atual: {', '.join(arquivos_faltando)}")
+print(f"Total: {len(arquivos_faltando)}")
