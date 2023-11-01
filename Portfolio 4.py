5dd3587bf6c48fb7701d217fa0a1ae5333d28360
mthehang <matheus@vts.com.br>
2023-11-01 14:02:56 -0300

    att port4

--- Portfolio 4.py
+++ Portfolio 4.py
@@ -5,7 +5,6 @@ lista_arquivos2 = ["IM0001.PDF", "IM0002.DCM", "IM0004.PDF"]
 arquivos_presentes = [arquivo for arquivo in lista_arquivos1 if arquivo in lista_arquivos2]
 arquivos_faltando = [arquivo for arquivo in lista_arquivos1 if arquivo not in lista_arquivos2]
 
-# Imprimir os resultados
 print(f"Arquivos presentes em ambos (sistema e backup): {', '.join(arquivos_presentes)}")
 print(f"Total: {len(arquivos_presentes)}\n")
 
